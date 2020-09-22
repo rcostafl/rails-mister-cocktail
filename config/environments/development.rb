a589433ef0976a20267629c8f9729114b5ce6449
Rafael Costa <rcostafl@gmail.com>
2020-09-22 11:23:36 -0300

    commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   # Active storage
-
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -62,6 +60,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-
 end
